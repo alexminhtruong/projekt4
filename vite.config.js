88512e740fb9a9c40b7e0f74e00874a47892411c
alexminhtruong <alextruong.1990@icloud.com>
2026-02-17 08:38:15 +0100

    fix: update base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: 'https://medieinstitutet.github.io/fed25d-js-inl-2-budget-app-alexminhtruong/',
+  base: 'https://alexminhtruong.github.io/projekt4/',
 });
